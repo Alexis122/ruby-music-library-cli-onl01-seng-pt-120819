d6307d218303fc6c46f9470a628b517e0e7b3bb4
Alexis Priore <alexispriore@gmail.com>
2020-02-21 20:42:08 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,10 +6,10 @@ def initialize(path)
   end
 
 def files
-Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/", '') }
+Dir.glob("#{path}/*").map {|file| file.gsub("#{path}/", '')}
   end
 
 def import
-files.each { |file| } song.create_from_filename(file) }
+files.each {|file| }Song.create_from_filename(file)}
   end
 end
